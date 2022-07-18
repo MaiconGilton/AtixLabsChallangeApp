b9011680741ef52e77df94f069e114842fa31203
Maicon Gilton <maicongilton@gmail.com>
2022-07-18 13:49:19 -0300

    add home, csv and png screens

---
+++ src/screens/CsvListScreen/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { View } from 'react-native';
+import { styles } from './styles';
+
+const CsvListScreen = () => {
+  return <View style={styles.container} />;
+};
+
+export default CsvListScreen;
--- src/screens/Navigator.tsx
+++ src/screens/Navigator.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import PngListScreen from './PngListScreen';
+import CsvListScreen from './CsvListScreen';
+import HomeScreen from './HomeScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -8,6 +11,22 @@ const Navigator = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
+        <Stack.Screen
+          name="HomeScreen"
+          component={HomeScreen}
+          options={{ title: 'Home' }}
+        />
+        <Stack.Screen
+          name="PngListScreen"
+          component={PngListScreen}
+          options={{ title: 'PNG List' }}
+        />
+
+        <Stack.Screen
+          name="CsvListScreen"
+          component={CsvListScreen}
+          options={{ title: 'CSV List' }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
