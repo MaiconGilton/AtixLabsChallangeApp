3f129c364a3f8dbd933781cdf321d02e3b540d8f
Maicon Gilton <maicongilton@gmail.com>
2022-07-18 13:48:35 -0300

    set app navigation

---
+++ src/screens/Navigator.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+const Navigator = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+export default Navigator;
