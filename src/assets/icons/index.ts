dc018b4523fcbb076dc20fedcce2abf46abb2d3e
Maicon Gilton <maicongilton@gmail.com>
2022-07-19 15:53:55 -0300

    add svg icons

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,4 @@
+export { default as CsvIcon } from './csv_icon.svg'
+export { default as PngIcon } from './png_icon.svg'
+export { default as UploadIcon } from './upload_icon.svg'
+export { default as ArrowForwardIcon } from './arrow_forward_icon.svg'
