335d2c159e1456715b47b11cc27f8b0e54befe4f
Maicon Gilton <maicongilton@gmail.com>
2022-07-19 16:33:15 -0300

    set default theme to navigator

--- src/screens/Navigator.tsx
+++ src/screens/Navigator.tsx
@@ -1,31 +1,48 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import PngListScreen from './PngListScreen';
 import CsvListScreen from './CsvListScreen';
 import HomeScreen from './HomeScreen';
+import useTheme from '../hooks/useTheme';
 
 const Stack = createNativeStackNavigator();
 
 const Navigator = () => {
+  const themeColors = useTheme()
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
+    <NavigationContainer
+      theme={{
+        ...DefaultTheme,
+        colors: {
+          ...DefaultTheme.colors,
+          background: themeColors.background,
+        }
+      }}
+    >
+      <Stack.Navigator
+        screenOptions={{
+          headerTintColor: 'white',
+          headerStyle: { backgroundColor: themeColors.primaryColor },
+        }}
+      >
         <Stack.Screen
           name="HomeScreen"
           component={HomeScreen}
-          options={{ title: 'Home' }}
+          options={{ title: 'Atix Labs Challange App' }}
         />
+
         <Stack.Screen
           name="PngListScreen"
           component={PngListScreen}
-          options={{ title: 'PNG List' }}
+          options={{ title: 'PNG Gallery' }}
         />
 
         <Stack.Screen
           name="CsvListScreen"
           component={CsvListScreen}
-          options={{ title: 'CSV List' }}
+          options={{ title: 'CSV Sheets' }}
         />
       </Stack.Navigator>
     </NavigationContainer>
